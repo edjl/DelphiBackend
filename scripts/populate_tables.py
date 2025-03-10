8497b0d32273595041f2b793f1f0576c0cb16fc7
Jade <jadeforrest1@outlook.com>
2025-03-09 20:38:15 -0400

    populate user table

---
+++ scripts/populate_tables.py
@@ -0,0 +1,22 @@
+from execute_sql import execute
+import random
+import sqlite3
+
+# connection = sqlite3.connect("../database/delphi_db_setup_schema.sql")
+# conn = connection.cursor()
+
+def populate_table(table_name, tuple_structure, tuples):
+    execute(f"""INSERT INTO {table_name} {tuple_structure} VALUES 
+        {tuples}""")
+    
+users = open('../database/usernames.txt', "r")
+lines = users.read().split('\n')
+tuples = []
+for id in range(0,200):
+    user = lines[id].replace(" ","")
+    email = user + '@gmail.com'
+    # print(email)
+    user = (id, lines[id], email, "1234", 1, random.randint(-10000,10000), random.randint(0,5), random.randint(0,5), random.randint(0,5), random.randint(0,100), random.randint(0,100), random.randint(0,100))
+    tuples.append(user)
+
+populate_table("users", "(id, username, email, password, admin, balance, bankruptcy_count, total_bets, curr_bets, total_credits_playing, total_credits_bet, total_credits_won)", tuples[0:199])
